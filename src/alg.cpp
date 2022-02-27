0e71b3aba88d7acc2a1ba58be5bf5455ca4637cf
Rtiemm <99262095+Rtiemm@users.noreply.github.com>
2022-02-27 16:35:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,48 +1,63 @@
 // Copyright 2022 NNTU-CS
+#include <math.h>
+
 #include <cstdint>
 
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
-  if (value % value == 0 && value % 1 == value) {
+  if (value > 1) {
+    for (int i = 2; i < value; i++)
+      if (value % i == 0) return false;
     return true;
-  } else {
+  } else
     return false;
-  }
 }
 
 uint64_t nPrime(uint64_t n) {
+  uint64_t count = 0;
   int num = 0;
-  int count = 0;
-  for (int i = 2; i < n; i++) {
-    if (i % i == 0 && i % 1 == i) {
-      count = count + 1;
-      if (num == n) {
+  while (count != n) {
+    for (int i = 2;; i++) {
+      int k = 0;
+      for (int j = 2; j < i; j++)
+        if (i % j == 0) k = k + 1;
+      if (k >= 1) {
+        count = count;
+      } else {
+        count = count + 1;
         num = i;
-        return num;
       }
-    } else {
-      return 0;
-    }
-  }
-  return 0;
-}
-
-uint64_t nextPrime(uint64_t value) {
-  uint64_t num = 0;
-  while (num % num != 0 && num % 1 != num) {
-    value = value + 1;
-    if (value % value == 0 && value % 1 == value) {
-      num = value;
     }
   }
   return num;
 }
 
+uint64_t nextPrime(uint64_t value) {
+  for (int i = 1; i <= 337446; i++) {
+    uint64_t num = 0;
+    int k = 0;
+    num = value + i;
+    for (int j = 2; j < num; j++)
+      if (num % j == 0) k = k + 1;
+    if (k >= 1) {
+      continue;
+    } else {
+      return num;
+    }
+  }
+  return 0;
+}
+
 uint64_t sumPrime(uint64_t hbound) {
   int sum = 0;
   for (int i = 2; i < hbound; i++) {
-    if (i % i == 0 && i % 1 == i) {
+    int k = 0;
+    for (int j = 2; j < i; j++)
+      if (i % j == 0) k = k + 1;
+    if (k >= 1) {
+      continue;
+    } else {
       sum = sum + i;
     }
   }
