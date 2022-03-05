3310eb1cbf914e341d85895d62ccc18c116c76bf
Rtiemm <99262095+Rtiemm@users.noreply.github.com>
2022-03-05 15:28:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,8 +10,9 @@ bool checkPrime(uint64_t value) {
     for (int i = 2; i < value; i++)
       if (value % i == 0) return false;
     return true;
-  } else
+  } else {
     return false;
+  }
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -31,37 +32,6 @@ uint64_t nPrime(uint64_t n) {
     }
   }
   return num;
-}
-
-uint64_t nextPrime(uint64_t value) {
-  for (int i = 1; i <= 337446; i++) {
-    uint64_t num = 0;
-    int k = 0;
-    num = value + i;
-    for (int j = 2; j < num; j++)
-      if (num % j == 0) k = k + 1;
-    if (k >= 1) {
-      continue;
-    } else {
-      return num;
-    }
-  }
-  return 0;
-}
-
-uint64_t sumPrime(uint64_t hbound) {
-  int sum = 0;
-  for (int i = 2; i < hbound; i++) {
-    int k = 0;
-    for (int j = 2; j < i; j++)
-      if (i % j == 0) k = k + 1;
-    if (k >= 1) {
-      continue;
-    } else {
-      sum = sum + i;
-    }
-  }
-  return sum;
 }
 
 uint64_t nextPrime(uint64_t value) {
