bb2745a6526ebe6ce1a81383f48746dcf587a973
Rtiemm <99262095+Rtiemm@users.noreply.github.com>
2022-03-05 17:50:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,26 +10,25 @@ bool checkPrime(uint64_t value) {
     for (int i = 2; i < value; i++)
       if (value % i == 0) return false;
     return true;
-  } else {
+  } else
     return false;
-  }
 }
 
 uint64_t nPrime(uint64_t n) {
   uint64_t count = 0;
   int num = 0;
+  int i = 2;
   while (count != n) {
-    for (int i = 2; num != i - 1; i++) {
-      int k = 0;
-      for (int j = 2; j < i; j++)
-        if (i % j == 0) k = k + 1;
-      if (k >= 1) {
-        count = count;
-      } else {
-        count = count + 1;
-        num = i;
-      }
+    int k = 0;
+    for (int j = 2; j < i; j++)
+      if (i % j == 0) k = k + 1;
+    if (k >= 1) {
+      count = count;
+    } else {
+      count = count + 1;
+      num = i;
     }
+    i++;
   }
   return num;
 }
@@ -51,10 +50,10 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  int sum = 0;
-  for (int i = 2; i < hbound; i++) {
-    int k = 0;
-    for (int j = 2; j < i; j++)
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i < hbound; i++) {
+    uint64_t k = 0;
+    for (uint64_t j = 2; j < i; j++)
       if (i % j == 0) k = k + 1;
     if (k >= 1) {
       continue;
