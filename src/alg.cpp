0646cd1364923eec22be08b32186e5ca4b5b6313
Rtiemm <99262095+Rtiemm@users.noreply.github.com>
2022-03-05 15:04:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,7 +18,7 @@ uint64_t nPrime(uint64_t n) {
   uint64_t count = 0;
   int num = 0;
   while (count != n) {
-    for (int i = 2; num != i; i++) {
+    for (int i = 2; num != i - 1; i++) {
       int k = 0;
       for (int j = 2; j < i; j++)
         if (i % j == 0) k = k + 1;
@@ -31,6 +31,37 @@ uint64_t nPrime(uint64_t n) {
     }
   }
   return num;
+}
+
+uint64_t nextPrime(uint64_t value) {
+  for (int i = 1; i <= 337446; i++) {
+    uint64_t num = 0;
+    int k = 0;
+    num = value + i;
+    for (int j = 2; j < num; j++)
+      if (num % j == 0) k = k + 1;
+    if (k >= 1) {
+      continue;
+    } else {
+      return num;
+    }
+  }
+  return 0;
+}
+
+uint64_t sumPrime(uint64_t hbound) {
+  int sum = 0;
+  for (int i = 2; i < hbound; i++) {
+    int k = 0;
+    for (int j = 2; j < i; j++)
+      if (i % j == 0) k = k + 1;
+    if (k >= 1) {
+      continue;
+    } else {
+      sum = sum + i;
+    }
+  }
+  return sum;
 }
 
 uint64_t nextPrime(uint64_t value) {
